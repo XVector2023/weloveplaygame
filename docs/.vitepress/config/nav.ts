01662a7c16f132fbdfcd4db097fc4e8e62b2aa2b
kevin-xvector <13604089+kevin-xvector@user.noreply.gitee.com>
2023-10-23 07:59:49 +0000

    信息更新
    Signed-off-by: kevin-xvector <13604089+kevin-xvector@user.noreply.gitee.com>

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -6,25 +6,20 @@ export const nav: DefaultTheme.Config['nav'] = [
     items: [
 		{
 			text: '法必帮-专业版',
-			link: 'https://lawvector.cn'
+			link: 'https://law.xvector.cn'
 		},
 		{ text: '法必帮-大众版', link: '/categories/xvector/index', activeMatch: '/categories/xvector/' },
     { text: 'AI开放平台', link: '/categories/xvector/index', activeMatch: '/categories/xvector/' },
     ]
   },
   {
-    text: '协议',
+    text: '用户协议',
     items: [
 		{ text: '法必帮用户协议', link: '/agreements/user/index', activeMatch: '/agreements/user/' },
 		{ text: '法必帮隐私协议', link: '/agreements/privacy/index', activeMatch: '/agreements/privacy/' },
 		{ text: '法必帮系列会员协议', link: '/agreements/member/index', activeMatch: '/agreements/member/' },
     ],
 	  activeMatch: '/agreements/'
-  },
-  {
-    text: '我的标签',
-    link: '/tags',
-    activeMatch: '/tags'
   },
   {
     text: '关于我们',
