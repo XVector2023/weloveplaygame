901d20d4689f8d068ed2d81f6e3492202974fb70
Kevin <>
2023-10-25 17:16:48 +0800

    更新文件 nav.ts 更新链接信息

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -8,12 +8,12 @@ export const nav: DefaultTheme.Config['nav'] = [
 			text: '法必帮-专业版',
 			link: 'https://law.xvector.cn'
 		},
-		{ text: '法必帮-大众版', link: '/categories/xvector/index', activeMatch: '/categories/xvector/' },
-    { text: 'AI开放平台', link: '/categories/xvector/index', activeMatch: '/categories/xvector/' },
+		{ text: '法必帮-大众版', link: '/categories/lawvector/ordinary', activeMatch: '/categories/lawvector/' },
+    { text: 'AI开放平台', link: '/categories/xvector/open', activeMatch: '/categories/xvector/' },
     ]
   },
   {
-    text: '用户协议',
+    text: '文档支持',
     items: [
 		{ text: '法必帮用户协议', link: '/agreements/user/index', activeMatch: '/agreements/user/' },
 		{ text: '法必帮隐私协议', link: '/agreements/privacy/index', activeMatch: '/agreements/privacy/' },
